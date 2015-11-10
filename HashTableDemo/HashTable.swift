36ca4fbf08c2d645c9ce68f030c65e7aaefe0fa2
Melinda Po <mellypo@gmail.com>
2015-11-10 12:09:37 -0800

    comment

--- HashTableDemo/HashTable.swift
+++ HashTableDemo/HashTable.swift
@@ -10,23 +10,44 @@ import Foundation
 
 
 
-
+//struct - pass by value
+//two generic values H is hashable not hashtable and T is the value stored
 struct HashTable<H: Hashable, T> {
-    var table = Array<SinglyLinkedList<H, T>?>()
+    //the linked list is optional generic type H, T
+    //table =new Array()
+    var table = Array<SinglyLinkedList<H, T>?>() // var can be declared anywhere
+    
     init() {
+        //the HashTable has an array of 10 lists
         for _ in 0...9 {
+            //table[0]= new SinglyLinkedList()
             table.append(SinglyLinkedList<H, T>())
         }
     }
+    //private function cannot be accessed outside of the hashtable
+    //takes a key, which is Hashable returns an Integer
     private func hasher(key: H) -> Int {
-        return abs(key.hashValue) % 10
+        //The protocol Hashable gives a 19 digit signed value
+        let inn = abs(key.hashValue) % 10
+        print(inn)
+        //4799450059375354168
+        //4799450059692192894
+        print(key.hashValue)
+        return inn // the key which the absolute values last digit
     }
+    // mutating is to change the variable table in the function
     mutating func setKey(key: H, withValue val: T) {
+        // the private function hasher is called to set the table index
         let bucket = hasher(key)
+        //upsertNodeWithKey get the key value and append the node to the singlylinklist of the key
         table[bucket]?.upsertNodeWithKey(key, AndValue: val)
     }
+    //the key is passed and the noe value is obtained
     func getValueAtKey(key: H) -> T? {
+        //get the hashable index
         let bucket = hasher(key)
+        //get the singlylistlint from the tables index and return the node value if exist
+        // the node value is returned without unwrapping so it is going to have Optional in it
         return table[bucket]?.findNodeWithKey(key)?.value
     }
 }
--- HashTableDemo/SinglyLinkedList.swift
+++ HashTableDemo/SinglyLinkedList.swift
@@ -24,22 +24,33 @@ class SinglyNode<H: Hashable, T> {
 
 struct SinglyLinkedList<H: Hashable, T> {
     var head = HeadNode<SinglyNode<H, T>>()
+    //takes the key value interger and return the Node
     func findNodeWithKey(key: H) -> SinglyNode<H, T>? {
+        //check if the Link list exist
         if var currentNode = head.next {
+            //traverse through the list and check if the key exists
             while currentNode.key != key {
                 if let nextNode = currentNode.next {
                     currentNode = nextNode
                 } else {
+                   // return nil if we are at the end of the list
                    return nil
                 }
             }
+            //return the current node
             return currentNode
         } else {
+            //return nil is there is no match found
             return nil
         }
     }
+    //key is Hashable and val is the generic type T the user can pass the value
+    //returns an Node
     func upsertNodeWithKey(key: H, AndValue val: T) -> SinglyNode<H, T> {
+        //check if the link list exist
         if var currentNode = head.next {
+            //traverse to the end of the node to the last node
+            //also check if the key already exist
             while currentNode.key != key {
                 if let nextNode = currentNode.next {
                     currentNode = nextNode
@@ -47,15 +58,20 @@ struct SinglyLinkedList<H: Hashable, T> {
                     break;
                 }
             }
+            //if key already exists replace the node with the value and return the node
             if currentNode.key == key {
                 currentNode.value = val
                 return currentNode
             } else {
+                //create a new node at the end of the list with the current(last node).next pointing to the new node
                 currentNode.next = SinglyNode<H, T>(key: key, value: val, nextNode: nil)
+                //return the new node force unwrap the newnode
                 return currentNode.next!
             }
         } else {
+            //if head.next is nil then create a new node to head.next
             head.next = SinglyNode<H, T>(key: key, value: val, nextNode: nil)
+            //return the new node force unwrap the newnode
             return head.next!
         }
     }
