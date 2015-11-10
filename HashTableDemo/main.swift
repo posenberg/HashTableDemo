4ddaf92995e0bb7e4f435eb27813b8b05e2bd9c8
Melinda Po <posenberg@users.noreply.github.com>
2015-11-10 12:01:45 -0800

    Update main.swift

--- HashTableDemo/main.swift
+++ HashTableDemo/main.swift
@@ -5,7 +5,7 @@
 //  Created by Ketul Patel on 6/8/15.
 //  Copyright (c) 2015 Ketul Patel. All rights reserved.
 //
-
+//this is main
 import Foundation
 var table = HashTable<String, String>()
 table.setKey("Hello", withValue: "Hi")
